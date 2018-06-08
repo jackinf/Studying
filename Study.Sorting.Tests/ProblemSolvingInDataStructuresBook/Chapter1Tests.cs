d1ae749bfb79fba1c87caf186fad3726e768dd73
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2018-06-08 22:09:16 +0300

    [chapter1] examples

---
+++ Study.Sorting.Tests/ProblemSolvingInDataStructuresBook/Chapter1Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Study.Sorting.ProblemSolvingInDataStructuresBook;
+
+namespace Study.Sorting.Tests.ProblemSolvingInDataStructuresBook
+{
+    public class Chapter1Tests
+    {
+        [Test]
+        public void PermutationTest()
+        {
+            var outputs = new List<string>();
+            Chapter01.Permutation(new List<int> {1, 2, 3, 4, 5}, 0, outputs);
+        }
+
+        [Test]
+        public void RotateArray()
+        {
+            var array = new List<int> {10, 20, 30, 40, 50, 60};
+            Chapter01.RotateArray(array, 2);
+            Assert.AreEqual(new List<int> {30, 40, 50, 60, 10, 20}, array);
+        }
+    }
+}
---
+++ Study.Sorting/ProblemSolvingInDataStructuresBook/Chapter01.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Study.Sorting.ProblemSolvingInDataStructuresBook
+{
+    public class Chapter01
+    {
+        /// <summary>
+        /// Euclid: GDC(n, m) == GDC(m, n%m)
+        /// </summary>
+        public static int Gdc(int m, int n)
+            => m < n ? Gdc(n, m) : m % n == 0 ? n : Gdc(n, m % n);
+
+        public static int Fibonacci(int n) => n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
+
+        public static void Permutation(List<int> arr, int i, List<string> outputs)
+        {
+            if (arr.Count == i)
+            {
+                outputs.Add(string.Join(", ", arr));
+                return;
+            }
+
+            for (var j = i; j < arr.Count; j++)
+            {
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+                Permutation(arr, i+1, outputs);
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+        }
+
+        public static void RotateArray(List<int> list, int positions)
+        {
+            void ReverseArray(int start, int end)
+            {
+                for (int i = start, j = end; i < j; i++, j--)
+                    (list[i], list[j]) = (list[j], list[i]);
+            }
+
+            ReverseArray(0, positions -1);
+            ReverseArray(positions, list.Count -1);
+            ReverseArray(0, list.Count -1);
+        }
+    }
+}
