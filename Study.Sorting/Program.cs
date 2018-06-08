9b87776700aeac4324607152e185e6ef91f683a8
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2018-06-08 19:57:05 +0300

    Add project files.

---
+++ Study.Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Study.Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Study.Sorting/SortingApps/BubbleSortApp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Study.Sorting.SortingApps
+{
+    public class BubbleSortApp
+    {
+        public static void Sort(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            for (int j = 0; j < list.Count - 1; j++)
+                if (list[j] > list[j + 1])
+                    (list[j], list[j + 1]) = (list[j + 1], list[j]);
+        }
+    }
+}
