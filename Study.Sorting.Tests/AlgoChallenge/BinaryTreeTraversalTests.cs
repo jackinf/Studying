2e816ec152567a4d84335eee107a5d2a1f46c17d
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2018-06-09 18:26:13 +0300

    [binary-tree-traversal]

---
+++ Study.Sorting.Tests/AlgoChallenge/BinaryTreeTraversalTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Study.Sorting.AlgoChallenge;
+
+namespace Study.Sorting.Tests.AlgoChallenge
+{
+    public class BinaryTreeTraversalTests
+    {
+        private BinaryTreeTraversal.Tree _tree;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tree = new BinaryTreeTraversal.Tree();
+            _tree.Insert(30);
+            _tree.Insert(35);
+            _tree.Insert(57);
+            _tree.Insert(15);
+            _tree.Insert(63);
+            _tree.Insert(49);
+            _tree.Insert(89);
+            _tree.Insert(77);
+            _tree.Insert(67);
+            _tree.Insert(98);
+            _tree.Insert(91);
+        }
+
+        [Test]
+        public void InorderTraversalTest()
+        {
+            var result = _tree.Inorder(_tree.Root).ToList();
+            Assert.AreEqual("15 30 35 49 57 63 67 77 89 91 98", string.Join(" ", result));
+        }
+
+        [Test]
+        public void PreorderTraversalTest()
+        {
+            var result = _tree.Preorder(_tree.Root);
+            Assert.AreEqual("30 15 35 57 49 63 89 77 67 98 91", string.Join(" ", result));
+        }
+
+        [Test]
+        public void PostorderTraversalTest()
+        {
+            var result = _tree.Postorder(_tree.Root);
+            Assert.AreEqual("15 49 67 77 91 98 89 63 57 35 30", string.Join(" ", result));
+        }
+    }
+}
---
+++ Study.Sorting/AlgoChallenge/BinaryTreeTraversal.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Study.Sorting.AlgoChallenge
+{
+    public class BinaryTreeTraversal
+    {
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public class Tree
+        {
+            public Node Root { get; set; }
+
+            public void Insert(int value)
+            {
+                Node node = new Node {Value = value};
+                var current = Root;
+                if (current == null)
+                {
+                    Root = node;
+                    return;
+                }
+
+                while (true)
+                {
+                    if (value < current.Value)
+                    {
+                        if (current.Left == null)
+                        {
+                            current.Left = node;
+                            return;
+                        }
+                        current = current.Left;
+                    }
+                    else
+                    {
+                        if (current.Right == null)
+                        {
+                            current.Right = node;
+                            return;
+                        }
+                        current = current.Right;
+                    }
+                }
+            }
+
+            public IEnumerable<int> Inorder(Node root)
+            {
+                if (root != null)
+                {
+                    foreach (var i in Inorder(root.Left)) yield return i;
+                    yield return root.Value;
+                    foreach (var i in Inorder(root.Right)) yield return i;
+                }
+            }
+
+            public IEnumerable<int> Preorder(Node root)
+            {
+                if (root != null)
+                {
+                    yield return root.Value;
+                    foreach (var i in Preorder(root.Left)) yield return i;
+                    foreach (var i in Preorder(root.Right)) yield return i;
+                }
+            }
+
+            public IEnumerable<int> Postorder(Node root)
+            {
+                if (root != null)
+                {
+                    foreach (var i in Postorder(root.Left)) yield return i;
+                    foreach (var i in Postorder(root.Right)) yield return i;
+                    yield return root.Value;
+                }
+            }
+        }
+    }
+}
