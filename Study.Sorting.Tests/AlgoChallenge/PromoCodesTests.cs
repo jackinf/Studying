dd19c3f273d40b4aec9fcdc6b8b96c6f007844ce
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2018-06-16 15:51:54 +0300

    [algo] Promo Codes

---
+++ Study.Sorting.Tests/AlgoChallenge/PromoCodesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Study.Algo.AlgoChallenge;
+
+namespace Study.Algo.Tests.AlgoChallenge
+{
+    public class PromoCodesTests
+    {
+        [Test]
+        public void Test00()
+        {
+            var sum = PromoCodes.GetPromoCodeFormulaSum(new [] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+            Assert.AreEqual(165, sum);
+        }
+
+        [Test]
+        public void Test01()
+        {
+
+            var codes = PromoCodes.Generate().Take(1000).ToList();
+            Assert.AreEqual(1000, codes.Count);
+
+            foreach (var code in codes)
+            {
+                var numbersArray = code.Select(x => int.Parse(x.ToString())).ToList();
+                Assert.AreEqual(9, numbersArray.Count);
+
+                var hasAnyLetterMoreOccurrencesThan2 = numbersArray
+                    .GroupBy(letter => letter, (letter, list) => new { Count = list.Count() })
+                    .Any(result => result.Count > 2);
+                Assert.IsFalse(hasAnyLetterMoreOccurrencesThan2);
+
+                var index = 9;
+                var sum = numbersArray.Aggregate(0, (a, b) => a + b * index--);
+                Assert.AreEqual(sum % 11, 0);
+            }
+        }
+    }
+}
---
+++ Study.Sorting.Tests/Combinatorics/VariationsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Study.Algo.Tests.Combinatorics
+{
+    public class VariationsTests
+    {
+        [Test]
+        public void Test01()
+        {
+            int[] ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Variations<int> variations = new Variations<int>(ints.ToList(), 3);
+            foreach (IList<int> variation in variations)
+                TestContext.Out.WriteLine(string.Join("", variation));
+        }
+    }
+}
